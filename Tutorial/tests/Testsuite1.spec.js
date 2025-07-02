ef89e6008ad94ae3e403f8289bb81eabf7b7df8e
Ashleysteph99 <ashleystephen999@example.com>
2025-07-02 14:41:46 +0530

    Starter code

--- Tutorial/tests/Testsuite1.spec.js
+++ Tutorial/tests/Testsuite1.spec.js
@@ -1,6 +1,9 @@
 const { test } = require('@playwright/test');
 
-test('Test case 1', async () => {
+test('Test case 1', async ({ page }) => {
+    // const context = browser.newContext(); /* for adding cookies*/
+    // const page = await context.newPage();
 
-    await open('');
+    await page.goto('');
+
 });
