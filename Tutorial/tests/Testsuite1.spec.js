e0b28a67b9e48944f4200cdde1f2437a0f5a1da4
Ashleysteph99 <ashleystephen999@example.com>
2025-07-04 12:01:21 +0530

    stored page title in a variable

--- Tutorial/tests/Testsuite1.spec.js
+++ Tutorial/tests/Testsuite1.spec.js
@@ -1,10 +1,13 @@
 const { test } = require('@playwright/test');
 
-test.only('Test case 1', async ({ page }) => {
+test.only('Check if Home page title is proper', async ({ page }) => {
     // const context = browser.newContext(); /* for adding cookies*/
     // const page = await context.newPage();
 
-    await page.goto('https://playace.co/?city=0');
-    console.log(`the page title is ${await page.title()}`);
+    await page.goto('https://playace.co/?city=0', { waitUntil: 'domcontentloaded' });
+    const titleofpage = await page.title();
+    console.log(`Title of the page is ${titleofpage}`);
+
+
 
 });
