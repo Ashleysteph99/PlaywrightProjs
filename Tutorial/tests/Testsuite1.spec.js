4ae90f1ea40de1b591b4379dfba56459e494284f
Ashleysteph99 <ashleystephen999@example.com>
2025-07-06 15:52:22 +0530

    added first test to check url title and correct error message

--- Tutorial/tests/Testsuite1.spec.js
+++ Tutorial/tests/Testsuite1.spec.js
@@ -1,13 +1,40 @@
-const { test } = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
+const { sign } = require('crypto');
 
-test.only('Check if Home page title is proper', async ({ page }) => {
+const url = "https://rahulshettyacademy.com/loginpagePractise/";
+
+test('Check if title and URL is proper', async ({ page }) => {
     // const context = browser.newContext(); /* for adding cookies*/
     // const page = await context.newPage();
 
-    await page.goto('https://playace.co/?city=0', { waitUntil: 'domcontentloaded' });
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
     const titleofpage = await page.title();
-    console.log(`Title of the page is ${titleofpage}`);
+    console.log(`Title of the page is "${titleofpage}"`);
+    console.log(await page.url());
 
+});
+
+test('Check if fields are present and editable', async ({ page }) => {
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
+    const usrnamefield = await page.locator('#username');
+    const usrpwdfield = await page.locator('#password');
+    const currenturl = await page.url();
+    const adminchckbox = await page.locator('.checkmark').nth(0);
+    const checkbox = await page.locator('#terms');
+    const signinbtn = await page.locator('#signInBtn');
+
+    await usrnamefield.fill("usrname");
+    await usrpwdfield.fill("usrname");
+
+    console.log(`User name field is editable? : ${await usrnamefield.isEditable()}`);
+    console.log(`Password field is editable?: ${await usrpwdfield.isEditable()}`);
+    console.log(`Current url is ${currenturl}`);
+    await adminchckbox.click();
+    await checkbox.click();
+    await signinbtn.click();
+
+    await expect(await page.locator('.alert.alert-danger')).toHaveText("Incorrect username/password.");
+    console.log((await page.locator('.alert.alert-danger').textContent() || '').trim().replace(/\s+/g, ' '));
 
 
 });
